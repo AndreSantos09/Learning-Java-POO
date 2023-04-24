8314e26c5374f97601d02cacab13c9110c1f363c
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-04-24 11:52:10 -0300

    delete

--- Lista04/src/ContaMain.java
+++
@@ -1,8 +0,0 @@
-public class ContaMain {
-        public static void main(String[] args) throws Exception {
-        ContaBancaria Andre = new ContaBancaria();
-        Andre.depositar(1000);    
-        Andre.conta();
-        }
-    }
-
