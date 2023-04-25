ef4a0736a1ef76a28c1fe3afa0bff3b90b45b6ef
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-04-25 18:19:34 -0300

    att

--- herançaPOO/Arvore/src/App.java
+++ herançaPOO/Arvore/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   // Visitante p1 = new Visitante();
+   // p1.setNome("andré");
+   // p1.setSexo("M");
+  //  p1.setIdade(25);
+  //  System.out.println(p1.toString());
+
+    Aluno a1 = new Aluno();
+    a1.setNome("André");
+    a1.setSexo("M");
+    a1.setIdade(25);
+    a1.setCurso("cc");
+    a1.setMatricula(541960);
+    a1.PagarMensalidade();
+    Bolsista b1 = new Bolsista();
+    b1.PagarMensalidade();
     }
 }
---
+++ herançaPOO/Arvore/src/Bolsista.java
@@ -0,0 +1,24 @@
+public final class Bolsista extends Aluno {
+    private int bolsa;
+
+    public void RenovarBolsa(){
+        System.out.println("bolsa renovada");
+    }
+
+    
+    public void PagarMensalidade(){
+        System.out.println("Mensalidade paga - bolsista");
+    }
+
+
+
+    public int getBolsa() {
+        return this.bolsa;
+    }
+
+    public void setBolsa(int bolsa) {
+        this.bolsa = bolsa;
+    }
+
+
+}
