e178aa1ee3bfb66e873d88379974a933d6c5953e
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-04-25 19:08:49 -0300

    att

--- Polimorfismo/Polimorfismo/src/App.java
+++ Polimorfismo/Polimorfismo/src/App.java
@@ -11,6 +11,24 @@ public class App {
         m.locomover();
         m.emitirSom();
 
+        r.setPeso(30);
+        r.setCorEscama("vermelho");
+        r.alimentar();
+        r.locomover();
+        r.emitirSom();
+
+        p.setPeso(20);
+        p.setCorEscama("preto");
+        p.alimentar();
+        p.locomover();
+        p.emitirSom();
+
+        a.setPeso(15);
+        a.setCorPena("branco");
+        a.alimentar();
+        a.locomover();
+        a.emitirSom();
+
 
     }
 }
